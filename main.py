ac944bc09c082120273538f35f9f7bbb49a3e1d8
Diego <toro.teutsch.d@gmail.com>
2020-06-05 00:02:19 -0400

    Implementing and rendering html templates to the Index and Hello_wold routes

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return '<h2>Hello World - Flask</h2>'
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
