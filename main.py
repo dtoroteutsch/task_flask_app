066e5b837df4c97cdd47d10f11f2ef7736318e23
Diego <toro.teutsch.d@gmail.com>
2020-06-04 23:37:12 -0400

    Defining the classic 'Hello World' route to test the server

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return '<h2>Hello World - Flask</h2>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
