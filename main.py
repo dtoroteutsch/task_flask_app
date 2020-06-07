cacf913deadcfd6f30e00bea17abf3334e131042
Diego <toro.teutsch.d@gmail.com>
2020-06-07 13:14:16 -0400

    Implementing Jinja2 to make more dynamic our web pages

--- main.py
+++ main.py
@@ -4,11 +4,28 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    context = {
+        'username': 'Diego'
+        }
+    return render_template('index.html', **context)
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return render_template('hello.html')
+    context = {
+        'username': 'Diego'
+        }
+    return render_template('hello.html', **context)
+
+@app.route('/courses', methods=['GET'])
+def show_courses():
+    context = {
+        'courses': [
+            {'name': 'Python', 'is_premiun': True},
+            {'name': 'Ruby', 'is_premiun': False},
+            {'name': 'Java', 'is_premiun': True}
+            ]
+        }
+    return render_template('courses.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
